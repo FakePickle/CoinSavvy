37c8f0e0e7f11fad4b8fb6f3faabeb5421631c01
noeltiju <noelab04@gmail.com>
2024-01-11 02:03:09 +0530

    Updated the final model to use yfinance datasets

--- final_model/final_predicted_values.py
+++ final_model/final_predicted_values.py
@@ -4,13 +4,19 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import csv
 import matplotlib.pyplot as plt
+import yfinance as yf
 
-# Load the dataset using pandas
-data = pd.read_csv('../datasets/training_dataset.csv')
-data_test = pd.read_csv('../datasets/testing_dataset.csv')
+ticker = "BTC-USD"
+start_date = "2018-02-01"
+end_date = "2022-01-01"
 
-X_features = ['open', "volumeto"]
-target_column = 'close'
+# Download historical data
+data = yf.download(ticker, start=start_date, end=end_date)
+data_test = yf.download(ticker, start="2022-02-01", end="2023-01-01")
+
+
+X_features = ['Open', "Volume"]
+target_column = 'Close'
 
 # Extract features (X_train) and target (y_train)
 X_train = data[X_features]
@@ -24,20 +30,22 @@ linear_model = LinearRegression()
 linear_model.fit(X_train, y_train) 
 
 predictions = linear_model.predict(X_test).tolist()
+time_column = data_test.index.tolist()
+
 
 with open("../datasets/final_predicted_values.csv", mode="w", newline="") as csv_file:
 
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['time', 'open', 'volumeto', 'predicted_close', 'close'])
-    
-    for index, row in data_test.iterrows():  # Use iterrows to iterate through DataFrame rows
-        time = row['time']
-        open_val = row['open']
-        volumeto = row['volumeto']
+    csv_writer.writerow(['Date', 'Open', 'Volume', 'Predicted Close', 'Actual Close'])
+    index = 0
+    for date, row in data_test.iterrows():  # Use iterrows to iterate through DataFrame rows
+        open_val = row['Open']
+        volumeto = row['Volume']
         predicted_close = predictions[index]
-        close = row['close']
+        close = row['Close']
 
-        csv_writer.writerow([time, open_val, volumeto, predicted_close, close])
+        csv_writer.writerow([date, open_val, volumeto, predicted_close, close])
+        index+=1
 
 mse_train = mean_squared_error(y_test, predictions)
 mse_test = mean_squared_error(y_train, linear_model.predict(X_train))
@@ -45,10 +53,10 @@ print(f"Mean Squared Error on Training Set: {mse_train}")
 print(f"Mean Squared Error on Test Set: {mse_test}")
 
 
-time_column =data_test['time']
 
 # Plotting the actual values
 plt.figure(figsize=(10, 6))
+plt.title('Actual vs Predicted Close Values')
 plt.plot(time_column, y_test, label='Actual', marker='o')
 
 # Plotting the predicted values
